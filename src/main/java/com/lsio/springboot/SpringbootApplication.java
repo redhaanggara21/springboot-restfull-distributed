b0db0395d94fdbbf18ea670edc043e4edf3b595c
redhaanggara21 <redhabayuanggara@gmail.com>
2023-01-11 17:19:49 +0700

    all function run login usescase with device info

--- src/main/java/com/lsio/springboot/SpringbootApplication.java
+++ src/main/java/com/lsio/springboot/SpringbootApplication.java
@@ -1,22 +1,8 @@
 package com.lsio.springboot;
 
-import java.time.format.DateTimeFormatter;
-import java.util.Arrays;
-
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
-import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer;
-
-import java.util.List;
-
-import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @SpringBootApplication
 @Configuration
@@ -29,34 +15,34 @@ public class SpringbootApplication {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
-	@Bean
-	public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer(){
-		return builder ->{
-			builder.simpleDateFormat(dateFormat);
-			builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
-			builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
-			builder.serializers(new ZonedDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
-		};
-	}
+	// @Bean
+	// public Jackson2ObjectMapperBuilderCustomizer jsonCustomizer(){
+	// 	return builder ->{
+	// 		builder.simpleDateFormat(dateFormat);
+	// 		builder.serializers(new LocalDateSerializer(DateTimeFormatter.ofPattern(dateFormat)));
+	// 		builder.serializers(new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+	// 		builder.serializers(new ZonedDateTimeSerializer(DateTimeFormatter.ofPattern(dateTimeFormat)));
+	// 	};
+	// }
 
-	@Bean
-	public CorsFilter corsFilter(){
-		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		final CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		List<String> originList = Arrays.asList("http://localhost:8080");
+	// @Bean
+	// public CorsFilter corsFilter(){
+	// 	final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	// 	final CorsConfiguration config = new CorsConfiguration();
+	// 	config.setAllowCredentials(true);
+	// 	List<String> originList = Arrays.asList("http://localhost:8080");
 
-		config.setAllowedOrigins(originList);
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("HEAD");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("DELETE");
-		config.addAllowedMethod("PATCH");
-		source.registerCorsConfiguration("/**", config);
+	// 	config.setAllowedOrigins(originList);
+	// 	config.addAllowedHeader("*");
+	// 	config.addAllowedMethod("OPTIONS");
+	// 	config.addAllowedMethod("HEAD");
+	// 	config.addAllowedMethod("GET");
+	// 	config.addAllowedMethod("PUT");
+	// 	config.addAllowedMethod("POST");
+	// 	config.addAllowedMethod("DELETE");
+	// 	config.addAllowedMethod("PATCH");
+	// 	source.registerCorsConfiguration("/**", config);
 
-		return new CorsFilter();
-	}
+	// 	return new CorsFilter();
+	// }
 }
